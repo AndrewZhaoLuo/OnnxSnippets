44d2809f16c7ba61758478740c69b371bd746426
Andrew Luo <andrew.zhao.luo@gmail.com>
2021-10-26 12:45:34 -0700

    onnx export examples

--- onnx_export/common.py
+++ onnx_export/common.py
@@ -1,7 +1,11 @@
-import torch
-
-from os import path
 import os
+from os import path
+
+import torch
+import torch._C as _C
+import torch.onnx
+
+TrainingMode = _C._onnx.TrainingMode
 
 
 def export_model(torch_model, x, name, dir="export/"):
@@ -24,12 +28,13 @@ def export_model(torch_model, x, name, dir="export/"):
             dir, f"{name}.onnx"
         ),  # where to save the model (can be a file or file-like object)
         export_params=True,  # store the trained parameter weights inside the model file
-        opset_version=10,  # the ONNX version to export the model to
-        do_constant_folding=True,  # whether to execute constant folding for optimization
+        opset_version=12,  # the ONNX version to export the model to
+        do_constant_folding=False,  # whether to execute constant folding for optimization
         input_names=["input"],  # the model's input names
         output_names=["output"],  # the model's output names
         dynamic_axes={
             "input": {0: "batch_size"},  # variable length axes
             "output": {0: "batch_size"},
         },
+        training=TrainingMode.TRAINING,
     )
---
+++ onnx_export/export_mobilenetv1_block.py
@@ -0,0 +1,66 @@
+import itertools
+from os import path
+
+import torch
+from pytorch.common.blocks import mobilenetv1_block
+from torch import nn
+
+from onnx_export import common
+
+
+class ExportMobilenetV1:
+    default_conditions = {
+        "in_channels": 512,
+        "out_channels": 512,
+        "spatial_dimension": 512,
+        "stride": 1,
+    }
+
+    sequential_conditions = {
+        "in_channels": [128, 256, 512],
+        "out_channels": [128, 256, 512],
+        "spatial_dimension": [128, 256, 512],
+    }
+
+    def get_all_conditions(self):
+        conditions = set()
+
+        for condition_name in self.sequential_conditions:
+            for v in self.sequential_conditions[condition_name]:
+                new_condition = self.default_conditions.copy()
+                new_condition[condition_name] = v
+                conditions.add(tuple(new_condition.items()))
+
+        return conditions
+
+    def export_model(
+        self, torch_model, ndim, features_in, spatial_dimensions, name, dir="export/"
+    ):
+        dims = [1, features_in] + [spatial_dimensions] * ndim
+
+        # Input to the model
+        x = torch.randn(*dims, requires_grad=True)
+        common.export_model(torch_model, x, name, dir=dir)
+
+    def export_mobilenetv1_block(
+        self,
+        in_channels,
+        out_channels,
+        stride,
+        spatial_dimension,
+        dir="./export",
+    ):
+
+        model = mobilenetv1_block.MobilenetV1Block(in_channels, out_channels, stride)
+        name = f"mobilenetv1_block_inc={in_channels}_spatial={spatial_dimension}_outc={out_channels}_stride={stride}"
+
+        self.export_model(model, 2, in_channels, spatial_dimension, name, dir=dir)
+
+
+if __name__ == "__main__":
+    exporter = ExportMobilenetV1()
+    conds = exporter.get_all_conditions()
+    for cond in conds:
+        print("Exporting:", cond)
+        cond = dict(cond)
+        exporter.export_mobilenetv1_block(**cond, dir="export/mobilenetv1_block")
