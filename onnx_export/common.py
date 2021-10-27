931234ae3a0e0bf3de7011745b0b1f41d5910405
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2021-10-27 11:36:45 -0700

    add const folding

--- onnx_export/common.py
+++ onnx_export/common.py
@@ -29,7 +29,7 @@ def export_model(torch_model, x, name, dir="export/"):
         ),  # where to save the model (can be a file or file-like object)
         export_params=True,  # store the trained parameter weights inside the model file
         opset_version=12,  # the ONNX version to export the model to
-        do_constant_folding=False,  # whether to execute constant folding for optimization
+        do_constant_folding=True,  # whether to execute constant folding for optimization
         input_names=["input"],  # the model's input names
         output_names=["output"],  # the model's output names
         dynamic_axes={
